0d4e8c203784dbbbad447c1b804ffbd9acadf412
Hajin-78 <hjc34856@gmail.com>
2024-12-07 23:46:43 +0900

    Update newthing.py

--- newthing.py
+++ newthing.py
@@ -1,24 +1,29 @@
 from transformers import AutoProcessor, AutoModelForImageTextToText
 from PIL import Image
-from googletrans import Translator  # 오류 뜨면 이거 설치 pip install googletrans==4.0.0-rc1
-import chagemodel
+from googletrans import Translator  # 설치 필요: pip install googletrans==4.0.0-rc1
+import chagemodel  # chagemodel.py 파일에서 modify_model 함수 사용
 
-# 로컬 경로에서 모델과 프로세서 불러오기
+# 모델과 프로세서 초기화
 processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
 model = AutoModelForImageTextToText.from_pretrained("Salesforce/blip-image-captioning-base")
-
-# 이미지 불러오기
-image_path = "./example_image/marat-musabirov.jpg"  # 이미지 파일 경로
-image = Image.open(image_path)
-
 model = chagemodel.modify_model(model)
 
-# 이미지 캡셔닝
-inputs = processor(image, return_tensors="pt")
-outputs = model.generate(**inputs, max_new_tokens=50)
-caption = processor.decode(outputs[0], skip_special_tokens=True)
-
-# 번역기
 translator = Translator()
-korean_caption = translator.translate(caption, src="en", dest="ko").text
-print("한글 캡션:", korean_caption)
+
+def generate_caption(image_path):
+
+    try:
+        # 이미지 로드
+        image = Image.open(image_path)
+
+        # 이미지 캡션 생성
+        inputs = processor(image, return_tensors="pt")
+        outputs = model.generate(**inputs, max_new_tokens=50)
+        caption = processor.decode(outputs[0], skip_special_tokens=True)
+
+        # 캡션 번역 (영어 -> 한국어)
+        translated_caption = translator.translate(caption, src="en", dest="ko").text
+
+        return caption, translated_caption
+    except Exception as e:
+        return "Error generating caption", str(e)
